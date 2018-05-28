f9cb89321f57a0524d7d38ad87a85e8b799bbc96
StYaphet <947670170@qq.com>
2018-05-29 07:25:51 +0800

    Initial Commit

---
+++ PDDImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDDImage
+//
+//  Created by 郝一鹏 on 2018/5/29.
+//  Copyright © 2018年 郝一鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
