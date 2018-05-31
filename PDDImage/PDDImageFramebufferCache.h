ce02ca57bb3799c1a45bb1064e47e8a48e7769e5
haoyipeng <haoyipeng@bytedance.com>
2018-05-31 09:57:25 +0800

    [add] 添加PDDImageFramebufferCache文件

---
+++ PDDImage/PDDImageFramebufferCache.h
@@ -0,0 +1,23 @@
+//
+//  PDDImageFramebufferCache.h
+//  PDDImage
+//
+//  Created by 郝一鹏 on 2018/5/31.
+//  Copyright © 2018年 郝一鹏. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <QuartzCore/QuartzCore.h>
+#import "PDDImageFramebuffer.h"
+
+@interface PDDImageFramebufferCache : NSObject
+
+// Framebuffer management
+- (PDDImageFramebuffer *)fetchFramebufferForSize:(CGSize)framebufferSize textureOptions:(GPUTextureOptions)textureOptions onlyTexture:(BOOL)onlyTexture;
+- (PDDImageFramebuffer *)fetchFramebufferForSize:(CGSize)framebufferSize onlyTexture:(BOOL)onlyTexture;
+- (void)retureFrambufferToCache:(PDDImageFramebuffer *)framebuffer;
+- (void)pergeAllUnassignedFramebuffers;
+- (void)addFramebufferToActiveImageCaptureList:(PDDImageFramebuffer *)framebuffer;
+- (void)removeFrambufferFromActiveImageCaptureList:(PDDImageFramebuffer *)framebuffer;
+
+@end
---
+++ PDDImage/PDDImageFramebufferCache.m
@@ -0,0 +1,13 @@
+//
+//  PDDImageFramebufferCache.m
+//  PDDImage
+//
+//  Created by 郝一鹏 on 2018/5/31.
+//  Copyright © 2018年 郝一鹏. All rights reserved.
+//
+
+#import "PDDImageFramebufferCache.h"
+
+@implementation PDDImageFramebufferCache
+
+@end
